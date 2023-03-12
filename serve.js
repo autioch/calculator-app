6ba0ffd40f8fe0945907cdab89589a81a1b9a8cf
Jakub Szczepaniak <jakub.szczepaniak@mtab.com>
2023-03-12 19:19:49 +0100

    Development fix.

--- serve.js
+++ serve.js
@@ -1,3 +1,5 @@
 // Used to serve the page to test with lighthouse
-// serve-local installed globally
+// npm i -g serve-local
+// npm link serve-local
+// node ./serve.js
 require('serve-local')(__dirname, 8080);
